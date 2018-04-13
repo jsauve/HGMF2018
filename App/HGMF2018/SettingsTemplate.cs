1ab77ae8729228985104a692646f352910b95dd5
Joe Sauve <joe@joesauve.com>
2018-04-13 11:00:32 -0500

    Fixed a few things

--- App/HGMF2018/SettingsTemplate.cs
+++ App/HGMF2018/SettingsTemplate.cs
@@ -3,7 +3,7 @@ namespace HGMF2018
 {
     public class Settings
     {
-        public const string UBER_CLIENT_ID = "UBER_CLIENT_ID";
-        public const string LYFT_CLIENT_ID = "LYFT_CLIENT_ID";
+        public const string UBER_CLIENT_ID = "UBER_CLIENT_ID_VALUE";
+        public const string LYFT_CLIENT_ID = "LYFT_CLIENT_ID_VALUE";
     }
 }
