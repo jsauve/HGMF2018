e1d1c7303d154a75dbb35db3287fa17c4f22459c
Joe Sauve <joe@joesauve.com>
2018-04-20 18:18:52 -0500

    Basic FCM working

---
+++ App/HGMF2018.Droid/PushNotifications/HGMFFirebaseMessagingService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Util;
+using Firebase.Messaging;
+using System.Collections.Generic;
+
+namespace HGMF2018.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class HGMFFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "HGMFFirebaseMessagingService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+
+            SendNotification(message.From, message.GetNotification().Body, message.Data);
+        }
+
+        void SendNotification(string messageTitle, string messageBody, IDictionary<string, string> data)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            foreach (string key in data.Keys)
+            {
+                intent.PutExtra(key, data[key]);
+            }
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new Notification.Builder(this)
+                .SetSmallIcon(Resource.Drawable.icon)
+                .SetContentTitle("Duluth Homegrown 2018")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
