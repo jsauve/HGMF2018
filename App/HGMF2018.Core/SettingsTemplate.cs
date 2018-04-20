bcf405ad7d4fb9b6bad129ebda50570dc9157ca1
Joe Sauve <joe@joesauve.com>
2018-04-20 10:34:22 -0500

    Project improvements

--- App/HGMF2018.Core/SettingsTemplate.cs
+++ App/HGMF2018.Core/SettingsTemplate.cs
@@ -2,9 +2,11 @@
 
 namespace HGMF2018.Core
 {
-    public class Settings
+    public static class Settings
     {
         public const string UBER_CLIENT_ID = "UBER_CLIENT_ID_VALUE";
         public const string LYFT_CLIENT_ID = "LYFT_CLIENT_ID_VALUE";
+        public const string AZURE_FUNCTION_IOSVERSION_API_KEY = "AZURE_FUNCTION_IOSVERSION_API_KEY_VALUE";
+        public const string AZURE_FUNCTION_ANDROIDVERSION_API_KEY = "AZURE_FUNCTION_ANDROIDVERSION_API_KEY_VALUE";
     }
 }
--- App/HGMF2018.Core/Utility/ExtensionMethods.cs
+++ App/HGMF2018.Core/Utility/ExtensionMethods.cs
@@ -1,5 +1,4 @@
 using System;
-using MvvmHelpers;
 using System.Linq;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
