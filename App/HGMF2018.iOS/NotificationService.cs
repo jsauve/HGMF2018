403cecac155fb76f65e88ed1d01c5c9aad330acb
Joe Sauve <joe@joesauve.com>
2018-04-21 15:51:16 -0500

    Moved to iOS 10 user notification APIs

---
+++ App/HGMF2018.iOS/NotificationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Foundation;
+using ObjCRuntime;
+using UserNotifications;
+
+namespace HGMF2018.iOS
+{
+    public class NotificationService : UNNotificationServiceExtension
+    {
+        protected NotificationService(NSObjectFlag t) : base(t)
+        {
+        }
+
+        protected internal NotificationService(IntPtr handle) : base(handle)
+        {
+        }
+
+		public override void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
+		{
+            base.DidReceiveNotificationRequest(request, contentHandler);
+		}
+	}
+}
---
+++ App/HGMF2018.iOS/UserNotificationCenterDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+using ObjCRuntime;
+using UserNotifications;
+
+namespace HGMF2018.iOS
+{
+    public class UserNotificationCenterDelegate : UNUserNotificationCenterDelegate
+    {
+        public UserNotificationCenterDelegate()
+        {
+        }
+
+		public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+		{
+            // Do something with the notification
+            Console.WriteLine("Active Notification: {0}", notification);
+
+            // Tell system to display the notification anyway or use
+            // `None` to say we have handled the display locally.
+            completionHandler(UNNotificationPresentationOptions.None);
+		}
+	}
+}
