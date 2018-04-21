21210435627f99ec55dfb276823d8b2fb30eb21a
Joe Sauve <joe@joesauve.com>
2018-04-20 21:49:57 -0500

    Azure-originated pushes working for iOS

--- App/HGMF2018.Droid/PushNotifications/HGMFFirebaseIIDService.cs
+++ App/HGMF2018.Droid/PushNotifications/HGMFFirebaseIIDService.cs
@@ -4,6 +4,7 @@ using Android.Util;
 using WindowsAzure.Messaging;
 using HGMF2018.Core;
 using System.Collections.Generic;
+using System;
 
 namespace HGMF2018.Droid
 {
@@ -22,7 +23,18 @@ namespace HGMF2018.Droid
         }
         void SendRegistrationToServer(string token)
         {
-            _Hub = new NotificationHub(Settings.AZURE_NOTIFICATIONHUB_NAME, Settings.AZURE_NOTIFICATIONHUB_CONNECTIONSTRING, this);
+            var hubName = String.Empty;
+            var hubConnectionString = String.Empty;
+
+#if DEBUG
+            hubName = Settings.AZURE_NOTIFICATIONHUB_NAME_SANDBOX;
+            hubConnectionString = Settings.AZURE_NOTIFICATIONHUB_CONNECTIONSTRING_SANDBOX;
+#else
+            hubName = Settings.AZURE_NOTIFICATIONHUB_NAME_PROD;
+            hubConnectionString = Settings.AZURE_NOTIFICATIONHUB_CONNECTIONSTRING_PROD;
+#endif
+
+            _Hub = new NotificationHub(hubName, hubConnectionString, this);
 
             var tags = new List<string>() { };
             var regID = _Hub.Register(token, tags.ToArray()).RegistrationId;
