a7331eddec4765b868feb301d7cf407ac93b612b
Joe Sauve <joe@joesauve.com>
2018-04-26 11:53:06 -0500

    Version bump: 1.0.22. Locked orientation to portrait. Improved error reporting.

--- App/HGMF2018.Droid/Services/LoggerService.cs
+++ App/HGMF2018.Droid/Services/LoggerService.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 using HGMF2018.Core;
 using HGMF2018.Droid;
-using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(LoggerService))]
@@ -12,6 +10,13 @@ namespace HGMF2018.Droid
 {
     public class LoggerService : ILoggerService
     {
+        IVersionRetrievalService _VersionRetrievalService;
+
+        public LoggerService()
+        {
+            _VersionRetrievalService = DependencyService.Get<IVersionRetrievalService>();
+        }
+
         void ILoggerService.LogException(Exception ex)
         {
             try
@@ -19,50 +24,11 @@ namespace HGMF2018.Droid
                 if (ex == null)
                     return;
 
-                MethodBase site = ex?.TargetSite;
-
-                var typeName = site?.ReflectedType?.DeclaringType?.Name;
-
-                var methodName = site?.ReflectedType?.Name;
-
-                StackFrame frame = (new StackTrace(ex, true))?.GetFrame(0);
-
-                int lineNumber = 0;
-                try { lineNumber = frame.GetFileLineNumber(); } catch { }
-
-                int columnNumber = 0;
-                try { columnNumber = frame.GetFileColumnNumber(); } catch { }
-
-                var additionalInfo = new Dictionary<string, string>();
-
-                if (!string.IsNullOrWhiteSpace(ex.Message))
-                    additionalInfo.Add("Message", (ex.Message.Length > 64) ? ex.Message.Substring(0, 64) : ex.Message);
-
-                if (!string.IsNullOrWhiteSpace(typeName))
-                    additionalInfo.Add("Type", (typeName.Length > 64) ? typeName.Substring(0, 64) : typeName);
-
-                if (!string.IsNullOrWhiteSpace(methodName))
-                    additionalInfo.Add("Method", (methodName.Length > 64) ? methodName.Substring(0, 64) : methodName);
-
-                if (lineNumber != 0)
-                    additionalInfo.Add("Line", lineNumber.ToString());
-
-                if (columnNumber != 0)
-                    additionalInfo.Add("Column", columnNumber.ToString());
-
-                var eventName = $"{ex.GetType().Name}: {ex.Message}";
-
-                if (eventName.Length > 256)
-                    eventName = eventName.Substring(0, 256);
-
-                // Azure Mobile Center doesn't yet support handled exceptions, so Im hijacking the Events feature of the service to record handled exceptions
-                Analytics.TrackEvent(eventName, additionalInfo);
+                Crashes.TrackError(ex, new Dictionary<string, string>() { {"Version", $"{_VersionRetrievalService.Version}" } });
             }
-            catch (Exception ex2)
+            catch
             {
-                // if we fail here, then there's nowher to report it, so just duck
-                System.Diagnostics.Debug.WriteLine(ex.Message);
-                System.Diagnostics.Debug.WriteLine(ex2.Message);
+                // if we fail here, then there's nowhere to report it, so just duck it
             }
         }
     }
